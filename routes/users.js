4b68ffdde6b5c840eb159cdb82a452948db045c0
MayanAdler <mayanmadler@gmail.com>
2023-03-26 12:28:47 +0300

    <day2-part1>

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router()
+const { addSymbol, welcome, dashboard } = require('../controllers/users/users.controller');
+const { addSymbolValidator } = require('../controllers/users/users.validators');
+
+const joi = require('../middlewares/joi');
+
+router.get('/welcome', welcome);
+router.get('/dashboard', dashboard);
+
+router.post('/symbol', joi(addSymbolValidator), addSymbol);
+
+module.exports = router;
